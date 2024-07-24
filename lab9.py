d0861407d8c67feee38a71ca4c31ef8c322bbdb0
juleskay-b <juliabush00@gmail.com>
2024-07-24 17:18:21 -0400

    Encode Function Update 1

--- lab9.py
+++ lab9.py
@@ -1,6 +1,13 @@
 def encode(password):
     new = ""
     for i in password:
-        new += str(int(i) + 3)
-    return new
+        if i == "7":
+            new += "0"
+        elif i == "8":
+            new += "1"
+        elif i == "9":
+            new += "2"
+        else:
+            new += str(int(i) + 3)
 
+    return new
