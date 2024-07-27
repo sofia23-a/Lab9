da773d0310ee39bab5bc93fd7e55d55c721405e6
sofia23-a <sofia.ahumada@ufl.edu>
2024-07-27 10:04:05 -0400

    Added main program

--- lab9.py
+++ lab9.py
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-#hello world
-=======
+
 def decode(password):
     new = ""
     for i in password:
@@ -14,7 +12,7 @@ def decode(password):
             new += str(int(i) - 3)
 
     return new
->>>>>>> 0e1897299491d6f7acbb9980a3165335f235b722
+
 
 def encode(password):
     encoded_password = ''
@@ -22,3 +20,43 @@ def encode(password):
         encoded_char = str(int(char) + 3)
         encoded_password += encoded_char
     return encoded_password
+
+def main():
+    stored_password = ''
+
+    while True:
+        print("\nMenu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+
+        try:
+            option = int(input("\nPlease enter an option: "))
+
+            if option == 1:
+                password = input("Please enter the password to encode: ")
+                stored_password = encode(password)
+                print("Your password has been encoded and stored!")
+
+            elif option == 2:
+                if stored_password:
+                    original_password = decode(stored_password)
+                    print(
+                        f"The encoded password is {stored_password}, and the original password is {original_password}.")
+                else:
+                    print("Please encode a password first.")
+
+            elif option == 3:
+                print("Exiting...")
+                break
+
+            else:
+                print("Invalid option. Please enter 1, 2, or 3.")
+
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+
+if __name__ == "__main__":
+    main()
